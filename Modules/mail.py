7da54e5d8f717b410b6ada93cda51c605c764728
Tomás Labuckas <81490429+TomLab17@users.noreply.github.com>
2023-05-20 17:19:18 -0300

    progreso en mail pero sigue sin funcionar

--- Modules/mail.py
+++ Modules/mail.py
@@ -1,19 +1,45 @@
 
-def obtenerMensaje ():
+def listarThreads(service):
+    results = service.messages().list(userId='me').execute()
+    messages = results.get('messages', [])
 
-    results = service.users().threads().list(userId='me').execute()
-    threads = results.get('threads', [])
-
-    if not threads:
-        print("no se encontraron hilosd e correo electrónico.")
+    if not messages:
+        print("no se encontraron hilos de correo electrónico.")
     else:
-        print("Hilos de correo electónico encontrados:")
+        print("Buscando Hilos:")
 
-    for thread in threads: 
-        print(f"ID del hilo: {thread['id']}")
+    return messages
 
-    return  
+def obtenerUltimosMails(cantEmails, service):
+    threads = listarThreads(service)
+    mensajes = []
+    #temporal hasta saber como limitar la busqueda
+    count = 0
+    for thread in threads:
+        if(count == 3):
+            break
+        mensajes.append(obtenerMensajes(thread, service))
+        count =+ 1
+    return mensajes
 
+def obtenerMensajes(thread, service):
+    results = service.threads().get(userId='me', id=thread["id"]).execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print("no se encontraron mensajes en los hilos.")
+
+    Froms = []
+    count = 0
+    for message in messages:
+        if(count == 10):
+            break
+        headers = message['payload']['headers']
+        for header in headers:
+            if(header['name'] == "From"):
+                Froms.append(header['value'])
+        count =+ 1
+    return Froms
 
 
 
--- Modules/terminal.py
+++ Modules/terminal.py
@@ -1,5 +1,3 @@
-def mostrarMailsComprometidos(mails):
-    print("Los mails comprometidos son los siguientes:")
-    for mail in mails:
-        print(mail+"\n")
-    print("Recomendamos eliminar estos mails o llevarlos a una casilla aparte y reportarlos")
+def mostrarMailComprometido(mail):
+    print("MAIL POTENCIALMENTE PELIGROSO: ")
+    print(mail+"\n")
