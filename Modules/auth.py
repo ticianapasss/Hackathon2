9e392820f7306f78f0e7c510c31ac70382b90d44
Tomás Labuckas <81490429+TomLab17@users.noreply.github.com>
2023-05-20 16:08:29 -0300

    cambios para blacklist

--- Modules/auth.py
+++ Modules/auth.py
@@ -43,17 +43,5 @@ def authenticate():
         print(f'An error occurred: {error}')
 
 
-results = service.users().threads().list(userId='me').execute()
-threads = results.get('threads', [])
-
-if not threads:
-    print("no se encontraron hilosd e correo electrónico.")
-else:
-    print("Hilos de correo electónico encontrados:")
-
-for thread in threads: 
-    print(f"ID del hilo: {thread['id']}")
-
-
 if __name__ == '__main__':
     authenticate()
