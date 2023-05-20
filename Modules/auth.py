28dafb5503c37af401c3484c943507ec18c38161
Tomás Labuckas <81490429+TomLab17@users.noreply.github.com>
2023-05-20 14:44:56 -0300

    return service

--- Modules/auth.py
+++ Modules/auth.py
@@ -37,21 +37,11 @@ def main():
     try:
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
-
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
+        return service.users()
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
         print(f'An error occurred: {error}')
 
 
 if __name__ == '__main__':
-    print("hola")
     main()
